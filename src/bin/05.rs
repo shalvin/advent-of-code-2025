8935216767b2a2e5d98927f3d9ad07b00443df6c
Shalvin Deo <shalvin.deo@essent.nl>
2026-01-07 23:09:50 +0100

    WIP

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -20,7 +20,28 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut ranges: Vec<(u64, u64)> = input
+        .lines()
+        .take_while(|line| !line.is_empty())
+        .map(|line| line.split_once('-').unwrap())
+        .map(|(begin, end)| (begin.parse::<u64>().unwrap(), end.parse::<u64>().unwrap()))
+        .collect();
+
+    ranges.sort_unstable_by(|(a_lower, _), (b_lower, _)| a_lower.cmp(b_lower));
+
+    let mut index = 0;
+    let mut count = 0;
+    for (lower, upper) in ranges.into_iter() {
+        if index <= lower {
+            count += (upper - lower) + 1;
+            index = upper;
+        } else if index > lower && index < upper {
+            count += upper - index;
+            index = upper;
+        }
+    }
+
+    Some(count)
 }
 
 #[cfg(test)]
@@ -36,6 +57,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 }
